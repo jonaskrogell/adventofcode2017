06030df40082bbc7d50c5888a255e4d6e05e965c
Jonas Krogell <jonas@krogell.se>
2017-12-13 12:28:59 +0100

    solved 8a

---
+++ 8.py
@@ -0,0 +1,47 @@
+import sys
+
+registry = {}
+
+stack = []
+for instruction in sys.stdin.read().strip().split('\n'):
+    instruction = instruction.split(' ')
+    reg = instruction[0]
+    op = instruction[1]
+    value = int(instruction[2])
+    statement = instruction[3]
+    comp_reg = instruction[4]
+    comp_operator = instruction[5]
+    comp_value = int(instruction[6])
+
+    print('Before:', registry)
+    print('Executing:', reg, op, value, statement, comp_reg, comp_operator, comp_value)
+    if reg not in registry:
+        registry[reg] = 0
+    if comp_reg not in registry:
+        registry[comp_reg] = 0
+
+    if op == 'inc':
+        diff = value
+    elif op == 'dec':
+        diff = -1 * value
+    else:
+        raise Exception('Unknown operator "%s"' % op)
+    if statement != 'if':
+        raise Exception('Unknown statement "%s"' % statement)
+
+    execute = False
+    if comp_operator == '==' and registry[comp_reg] == comp_value:execute = True
+    elif comp_operator == '>'  and registry[comp_reg] >  comp_value: execute = True
+    elif comp_operator == '>=' and registry[comp_reg] >= comp_value: execute = True
+    elif comp_operator == '<'  and registry[comp_reg] <  comp_value: execute = True
+    elif comp_operator == '<=' and registry[comp_reg] <=  comp_value: execute = True
+    elif comp_operator == '!=' and registry[comp_reg] != comp_value: execute = True
+
+    if execute:
+        print('Updating reg...')
+        registry[reg] += diff
+
+    print('After:', registry)
+
+print('Registry:', registry)
+print('Largest value in registry:', max(registry.values()))
