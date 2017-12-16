43fd6c315815ade02017ef96b205ab642a313d63
Jonas Krogell <jonas@krogell.se>
2017-12-16 11:43:28 +0100

    solved 16b in 22 seconds

--- 16.py
+++ 16.py
@@ -11,17 +11,33 @@ def swap(programs, p1, p2):
     programs[p2] = tmp
 
 
+moves = []
 for move in sys.stdin.read().strip().split(','):
-    print(programs)
-    print(move)
-    if move[0] == 's':
-        size = -1 * int(move[1:])
-        programs = programs[size:] + programs[0:size]
-    elif move[0] == 'x':
-        x1, x2 = move[1:].split('/')
-        swap(programs, int(x1), int(x2))
-    elif move[0] == 'p':
-        x1, x2 = move[1:].split('/')
-        swap(programs, programs.index(x1), programs.index(x2))
+    moves.append(move)
 
-print(''.join(programs))
+states = {}
+
+counter = 1
+loop = 1000000000
+while counter < loop:
+    state = ''.join(programs)
+    if counter < (loop - 1000) and state in states:
+        counter += states[state]
+        continue
+    elif counter > 100 and state not in states:
+        states[state] = counter
+
+    for move in moves:
+        if move[0] == 's':
+            size = -1 * int(move[1:])
+            programs = programs[size:] + programs[0:size]
+        elif move[0] == 'x':
+            x1, x2 = move[1:].split('/')
+            swap(programs, int(x1), int(x2))
+        elif move[0] == 'p':
+            x1, x2 = move[1:].split('/')
+            swap(programs, programs.index(x1), programs.index(x2))
+    counter += 1
+
+print('State table:', len(states))
+print(counter, ''.join(programs))
